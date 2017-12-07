13c374d18ea22776b3622a1fba71540f8ab303b5
azamat9812 <azamat9812@gmail.com>
2017-12-08 00:27:48 +0500

    port

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const bodyParaser   = require('body-parser');
 const app           = express();
 app.use(bodyParaser.json());
 
-const port = 3000;
+const port = 8080;
 MongoClient.connect(db.url, (err, database) => {
     if(err) throw err;
     require('./app/routes') (app, database);
-    app.listen(port, () => {
+    app.listen(process.env.port || port, () => {
         console.log('We are live on: ' + port);
     })
 });
